bac34f5ef77dc576b945881549064675d308e8d7
mlorber <87738509+mlorberdev@users.noreply.github.com>
2022-08-26 15:25:48 -0400

    updated content

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -2,14 +2,31 @@ window.onload = function () {
   let d = document.getElementsByTagName("main")[0].innerHTML;
   let l = document.getElementById("wall-l");
   l.innerText = d;
-
-  let t = document.getElementById("my-spec");
-  let u = `FULL-STACK DEVELOPER & EDUCATOR `;
-  let n = -1;
   
-  const int = setInterval(() => {
-    let c = u.charAt(n);
-    n === u.length ? clearInterval(int) : n++;
-    c === ' ' ? t.innerHTML += '<span>&nbsp;</span>' : t.innerHTML += c;
-  }, 80);
+  const ldr = document.getElementById("loader");
+  const y = document.getElementById("counter");
+  let x = 10;
+  const w = setInterval(() => {
+    y.innerText = x + "%";
+    if (x < 100) x += 2;
+    else {
+      clearInterval(w);
+      ldr.style.opacity = "0";
+      setTimeout(() => { ldr.remove(); whois() }, 300);
+    }
+  }, 50);
+
+  function whois() {
+
+    let t = document.getElementById("my-spec");
+    let u = `FULL-STACK DEVELOPER & EDUCATOR `;
+    let n = -1;
+
+    const int = setInterval(() => {
+      let c = u.charAt(n);
+      n === u.length ? clearInterval(int) : n++;
+      c === ' ' ? t.innerHTML += '<span>&nbsp;</span>' : t.innerHTML += c;
+    }, 80);
+  }
+
 }

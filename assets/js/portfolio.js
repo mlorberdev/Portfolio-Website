553ebe15514412d2ea35fa879edbf54dd0cfc4c5
mlorber <87738509+mlorberdev@users.noreply.github.com>
2022-08-27 20:05:39 -0400

    responsive updates

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,70 +1,39 @@
 window.addEventListener('load', () => {
-  const carousels = document.querySelectorAll('.carousel');
-
-  for (let i = 0; i < carousels.length; i++) {
-    carousel(carousels[i]);
-  }
-});
-
-function carousel(root) {
-  const figure = root.querySelector('figure');
-  const nav = root.querySelector('nav');
-  const images = figure.children;
+  const c = document.getElementById("carousel");
+  const fig = c.querySelector('figure');
+  const nav = c.querySelector('nav');
+  const images = fig.children;
   const n = images.length;
-  const gap = 40;
-  const bfc = ('bfc' in root.dataset);
-  const theta = 2 * Math.PI / n;
-  let img = 0;
+  const t = 2 * Math.PI / n;
+  let index = 0;
 
-  setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+  window.addEventListener('resize', setupCarousel);
 
-  window.addEventListener('resize', () => setupCarousel(n, parseFloat(getComputedStyle(images[0]).width)));
+  function setupCarousel() {
 
-  setupNavigation();
-
-  function setupCarousel(n, s) {
-    const apothem = s / (2 * Math.tan(Math.PI / n));
-    figure.style.transformOrigin = `50% 50% ${-apothem}px`;
+    let gap = innerWidth > 768 ? 40 : 20;
+    const apothem = parseFloat(getComputedStyle(images[0]).width) / (2 * Math.tan(Math.PI / n));
+    fig.style.transformOrigin = `50% 50% ${-apothem}px`;
 
     for (let i = 0; i < n; i++) {
-      
+      images[i].addEventListener("click", openProject);
       images[i].style.padding = `${gap}px`;
-    }
-    
-    for (let i = 1; i < n; i++) {
-      
-      images[i].style.transformOrigin = `50% 50% ${-apothem}px`;
-      images[i].style.transform = `rotateY(${i * theta}rad)`;
-    }
-    
-    if (bfc) {
-      for (let i = 0; i < n; i++) {
-        // if (window.CP.shouldStopExecution(4)) break; if (window.CP.shouldStopExecution(3)) break;
-        images[i].style.backfaceVisibility = 'hidden';
+      if (i > 0) {
+        images[i].style.transformOrigin = `50% 50% ${-apothem}px`;
+        images[i].style.transform = `rotateY(${i * t}rad)`;
       }
     }
-    turn(img);
+  } setupCarousel();
+
+  void function setupNavigation() {
+    nav.addEventListener('click', (e) => {
+      e.target.classList.contains("next") ? index++ : index--;
+      fig.style.transform = `rotateY(${index * -t}rad)`;
+    });
+  }();
+
+  function openProject() {
+    console.log(this.src);
   }
 
-  function setupNavigation() {
-    nav.addEventListener('click', onClick, true);
-
-    function onClick(e) {
-      e.stopPropagation();
-
-      var t = e.target;
-      if (t.tagName.toUpperCase() !== 'BUTTON')
-        return;
-
-      t.classList.contains("next") ? img++ : img--;
-
-      turn(img);
-    }
-
-  }
-
-  function turn(imageIndex) {
-    figure.style.transform = `rotateY(${imageIndex * -theta}rad)`;
-  }
-
-}
+});

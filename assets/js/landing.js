adad042d6ab129e45aa1f65ea6dec5424bb269b7
mlorber <87738509+mlorberdev@users.noreply.github.com>
2022-08-28 11:25:21 -0400

    content update

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,7 +1,7 @@
 // HOMEPAGE ANIMATION
 !function () {
   // populate landing page wall with document's html
-  document.getElementById("wall-l").innerText = document.getElementsByTagName("main")[0].innerHTML;
+  document.getElementById("wall").innerText = document.getElementsByTagName("main")[0].innerHTML;
   // remove noscript tag if JS enabled
   document.getElementById("ns").style.display = "none";
   // remove device rotate if in landscape
@@ -14,9 +14,10 @@
   // run loading animation
   const ldr = document.getElementById("loader");
   const y = document.getElementById("counter");
+  y.animate([{ color: 'lightgray' }, { color: 'darkgray' }], { duration: 3000, iterations: 1 });
   let x = 10;
   const w = setInterval(() => {
-    y.innerText = x + "%";
+    y.innerText = x;
     if (x < 100) x += 2;
     else {
       clearInterval(w);
@@ -27,7 +28,7 @@
   // run whois text animation
   async function whois() {
     let t = document.getElementById("my-spec");
-    let u = `FULL-STACK DEVELOPER & EDUCATOR `;
+    let u = `FULL-STACK DEVELOPER @ SD IT SOLUTIONS `;
     let n = -1;
     const int = setInterval(() => {
       let c = u.charAt(n);

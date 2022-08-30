13579bf49d9cfd859ba3a04487f46b94bf1e5518
mlorber <87738509+mlorberdev@users.noreply.github.com>
2022-08-30 12:46:28 -0400

    content update

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,20 +1,21 @@
-// HOMEPAGE ANIMATION
 !function () {
-  // populate landing page wall with document's html
+  // Populate landing page wall with document's html
   document.getElementById("wall").innerText = document.getElementsByTagName("main")[0].innerHTML;
-  // remove noscript tag if JS enabled
+
+  // Remove noscript tag if JS enabled
   document.getElementById("ns").style.display = "none";
-  // remove device rotate if in landscape
+
+  // Remove device rotate if in landscape
   function resize() {
     let scr = document.getElementById("landscape");
     innerWidth > innerHeight ? scr.style.display = "none" : scr.style.display = "flex";
   }
-  window.addEventListener("resize", () => { resize, scr });
+  window.addEventListener("resize", () => { resize(); scroll() });
   resize();
-  // run loading animation
+
+  // Run loading animation
   const ldr = document.getElementById("loader");
   const y = document.getElementById("counter");
-  y.animate([{ color: 'lightgray' }, { color: 'var(--accent)' }], { duration: 3000, iterations: 1 });
   let x = 10;
   const w = setInterval(() => {
     y.innerText = `${x}%`;
@@ -22,26 +23,17 @@
     else {
       clearInterval(w);
       ldr.style.opacity = "0";
-      setTimeout(() => { ldr.remove(); whois() }, 300);
+      setTimeout(() => { ldr.remove() }, 300);
     }
   }, 50);
-  // run whois text animation
-  async function whois() {
-    let t = document.getElementById("my-spec");
-    let u = `FULL-STACK DEVELOPER @ SD IT SOLUTIONS `;
-    let n = -1;
-    const int = setInterval(() => {
-      let c = u.charAt(n);
-      n === u.length ? clearInterval(int) : n++;
-      c === ' ' ? t.innerHTML += '<span>&nbsp;</span>' : t.innerHTML += c;
-    }, 80);
-  }
-  // set scroll into view positions
-  void function scr() {
+
+  // Set scroll into view positions for navs and observer
+  function scroll() {
     const articles = document.querySelectorAll("article");
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => document.getElementsByName(entry.target.id)[0].style.borderBottom = entry.isIntersecting ? "1px solid var(--accent)" : "0px solid var(--accent)");
     }, { root: null, rootMargin: '0px', threshold: 1 });
     articles.forEach(article => observer.observe(article));
-  }();
+  } scroll();
+
 }();
